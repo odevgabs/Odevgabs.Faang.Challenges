cb98b78156b6d0704017d85fe5495bd013974094
Gabriel Rodrigues <grfgabriel.ti@gmail.com>
2023-09-30 19:53:51 -0300

    remove not used files

--- src/App/Challenges/132Pattern.cs
+++ src/App/Challenges/132Pattern.cs
@@ -2,7 +2,20 @@
 
 public class _132Pattern
 {
-  public bool Find132pattern(int[] nums) {
-        return false;
+  public bool Find132pattern(int[] nums)
+  {
+    for (int i = 0; i < nums.Length; i++)
+    {
+      for (int j = i + 1; j < nums.Length; j++)
+      {
+        for (int k = j + 1; k < nums.Length; k++)
+        {
+          if (nums[i] < nums[k])
+            if (nums[k] < nums[j])
+              return true;
+        }
+      }
     }
+    return false;
+  }
 }
--- tests/UnitTests/Challenges/132PatternTests.cs
+++ tests/UnitTests/Challenges/132PatternTests.cs
@@ -16,7 +16,8 @@ public class _132PatternTests
     [InlineData(new int[]{1,2,3,4}, false)]
     [InlineData(new int[]{3,1,4,2}, true)]
     [InlineData(new int[]{-1,3,2,0}, true)]
-    private void ShouldValidatePattern(int[] input, bool isPattern)
+    [InlineData(new int[]{3,5,0,3,4}, true)]
+    public void ShouldValidatePattern(int[] input, bool isPattern)
     {
         bool isPatternResult = __132Pattern.Find132pattern(input);
         isPatternResult.Should().Be(isPattern);
